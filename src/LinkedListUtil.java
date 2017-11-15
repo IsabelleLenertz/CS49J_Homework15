5e1b7869ab9e19c9c9dbb756bd4240783493dc11
Isabelle Delmas <isabelle.lenertz@gmail.com>
2017-11-15 14:13:52 -0800

    15A done, 15B started

--- src/LinkedListUtil.java
+++ src/LinkedListUtil.java
@@ -1,11 +1,30 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Stack;
 
 public class LinkedListUtil {
+	/**
+	 * Remove every nth node in the LinkedList of Strings. Start the removal with the nth not the 0th node
+	 * @param strings list of strings
+	 * @param n positions to remove
+	 */
 	public static void shrink(LinkedList<String> strings, int n) {
-		//removes every nth node in the LinkedList of Strings. Start the removal with the nth not the 0th node
+		
+		for(int i =1; i < strings.size(); i++) {
+			if(i%n == 0) {
+				strings.remove(i);
+			}
+		}
 	}
 	public static LinkedList<String> reverse(LinkedList<String> strings) {
-		//return a LinkedList in which the the order of the elements is reversed.
-		return null;
+		LinkedList<String> reversedList = new LinkedList<>();
+		Stack<String> stack = new Stack<>();
+		ListIterator<String> it = strings.listIterator();
+		
+		while(it.hasNext()) {
+			
+		}
+		
+		return reversedList;
 	}
 }
--- src/RectangleSorter.java
+++ src/RectangleSorter.java
@@ -2,15 +2,28 @@ import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Sort rectangles
+ * @author isabelle delmas
+ *
+ */
 public class RectangleSorter {
+	/**
+	 * Sort rectangles by their area (small area first)
+	 * @param list list of rectangles
+	 */
 	public static void sortByArea(ArrayList<Rectangle> list) {
 		Collections.sort(list, (r1, r2) -> 
 			(int)(r1.getHeight()*r2.getWidth() - r2.getHeight()*r2.getWidth())
 		);
 	}
 	
+	/**
+	 * Sort rectangles by their perimeter (large perimeter first)
+	 * @param list list of rectangles
+	 */
 	public static void sortByDescendingPerimeter (ArrayList<Rectangle> list) {
-		Collections.sort(list, (r1, r2)-> 
+		Collections.sort(list, (r2, r1)-> 
 			(int)((r1.getHeight() + r1.getWidth()) - (r2.getHeight() + r2.getWidth()))
 		);
 	}
